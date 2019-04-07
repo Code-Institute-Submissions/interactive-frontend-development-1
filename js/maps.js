3acd1a36d4818c076845b0c09f04a43a4af1f5a6
Marius Dumitru <creion.m@gmail.com>
2019-04-07 13:18:36 +0000

    limited the search to 5 countries individually or togetter

--- js/maps.js
+++ js/maps.js
@@ -1,272 +1,82 @@
-/*function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: {
-            lat: 35.668477,
-            lng: 139.4606832
-        }
-    });
-
-    // Create an array of alphabetical characters used to label the markers.
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    var locations = [
-        { lat: 35.668477, lng: 139.4606832 },  //Tokyo
-        { lat: 40.6971745, lng: -74.5401066 },  // New York
-        { lat: -22.9118825, lng: -43.1728965 },  // Rio de Janeiro
-        { lat: 51.5287352, lng: -0.3817756 },  // London
-        { lat: -33.913129, lng: 18.095636 }  // Cape Town
-    ];
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
-*/
-
-var map, places, infoWindow;
-var markers = [];
-var autocomplete;
-var countryRestrict = { 'country': 'New York' };
-var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
-var hostnameRegexp = new RegExp('^https?://.+?/');
-
-var countries = {
-    'Tokyo': {
-        center: { lat: 35.652832, lng: 139.839478 },
-        zoom: 10
-    },
-    'New York': {
-        center: { lat: 40.730610, lng: -73.935242 },
-        zoom: 10
-    },
-    'Rio de Janeiro': {
-        center: { lat: -22.970722, lng: -43.182365 },
-        zoom: 10
-    },
-    'London': {
-        center: { lat: 51.509865, lng: -0.118092 },
-        zoom: 10
-    },
-    'Cape Town': {
-        center: { lat: -33.918861, lng: 18.423300 },
-        zoom: 10
-    }
-};
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: countries['New York'].zoom,
-        center: countries['New York'].center,
-        mapTypeControl: false,
-        panControl: false,
-        zoomControl: false,
-        streetViewControl: false
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 50.064192, lng: -130.605469 },
+        zoom: 3
+    });
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+    var countries = document.getElementById('country-selector');
+
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(card);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Set initial restrict to the greater list of countries.
+    autocomplete.setComponentRestrictions({ 'country': ['JP', 'US', 'BR', 'GB', 'ZA'] });
+
+    // Specify only the data fields that are needed.
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
     });
 
-    infoWindow = new google.maps.InfoWindow({
-        content: document.getElementById('info-content')
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        }
+        else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17); // Why 17? Because it looks good.
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindowContent.children['place-icon'].src = place.icon;
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map, marker);
     });
 
-    // Create the autocomplete object and associate it with the UI input control.
-    // Restrict the search to the default country, and to place type "cities".
-    autocomplete = new google.maps.places.Autocomplete(
-        /** @type {!HTMLInputElement} */
-        (
-            document.getElementById('autocomplete')), {
-            types: ['(cities)'],
-            componentRestrictions: countryRestrict
+    // Sets a listener on a given radio button. The radio buttons specify
+    // the countries used to restrict the autocomplete search.
+    function setupClickListener(id, countries) {
+        var radioButton = document.getElementById(id);
+        radioButton.addEventListener('click', function() {
+            autocomplete.setComponentRestrictions({ 'country': countries });
         });
-    places = new google.maps.places.PlacesService(map);
-
-    autocomplete.addListener('place_changed', onPlaceChanged);
-
-    // Add a DOM event listener to react when the user selects a country.
-    document.getElementById('country').addEventListener(
-        'change', setAutocompleteCountry);
-}
-
-// When the user selects a city, get the place details for the city and
-// zoom the map in on the city.
-function onPlaceChanged() {
-    var place = autocomplete.getPlace();
-    if (place.geometry) {
-        map.panTo(place.geometry.location);
-        map.setZoom(15);
-        search();
-    }
-    else {
-        document.getElementById('autocomplete').placeholder = 'Enter a city';
-    }
-}
-
-// Search for hotels in the selected city, within the viewport of the map.
-function search() {
-    var search = {
-        bounds: map.getBounds(),
-        types: ['lodging'],
-    };
-
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            clearResults();
-            clearMarkers();
-            // Create a marker for each hotel found, and
-            // assign a letter of the alphabetic to each marker icon.
-            for (var i = 0; i < results.length; i++) {
-                var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-                var markerIcon = MARKER_PATH + markerLetter + '.png';
-                // Use marker animation to drop the icons incrementally on the map.
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                // If the user clicks a hotel marker, show the details of that hotel
-                // in an info window.
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-                addResult(results[i], i);
-            }
-        }
-    });
-}
-
-function clearMarkers() {
-    for (var i = 0; i < markers.length; i++) {
-        if (markers[i]) {
-            markers[i].setMap(null);
-        }
-    }
-    markers = [];
-}
-
-// Set the country restriction based on user input.
-// Also center and zoom the map on the given country.
-function setAutocompleteCountry() {
-    var country = document.getElementById('country').value;
-    if (country == 'all') {
-        autocomplete.setComponentRestrictions({ 'country': [] });
-        map.setCenter({ lat: 15, lng: 0 });
-        map.setZoom(2);
-    }
-    else {
-        autocomplete.setComponentRestrictions({ 'country': country });
-        map.setCenter(countries[country].center);
-        map.setZoom(countries[country].zoom);
-    }
-    clearResults();
-    clearMarkers();
-}
-
-function dropMarker(i) {
-    return function() {
-        markers[i].setMap(map);
-    };
-}
-
-function addResult(result, i) {
-    var results = document.getElementById('results');
-    var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-    var markerIcon = MARKER_PATH + markerLetter + '.png';
-
-    var tr = document.createElement('tr');
-    tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
-    tr.onclick = function() {
-        google.maps.event.trigger(markers[i], 'click');
-    };
-
-    var iconTd = document.createElement('td');
-    var nameTd = document.createElement('td');
-    var icon = document.createElement('img');
-    icon.src = markerIcon;
-    icon.setAttribute('class', 'placeIcon');
-    icon.setAttribute('className', 'placeIcon');
-    var name = document.createTextNode(result.name);
-    iconTd.appendChild(icon);
-    nameTd.appendChild(name);
-    tr.appendChild(iconTd);
-    tr.appendChild(nameTd);
-    results.appendChild(tr);
-}
-
-function clearResults() {
-    var results = document.getElementById('results');
-    while (results.childNodes[0]) {
-        results.removeChild(results.childNodes[0]);
-    }
-}
-
-// Get the place details for a hotel. Show the information in an info window,
-// anchored on the marker for the hotel that the user selected.
-function showInfoWindow() {
-    var marker = this;
-    places.getDetails({ placeId: marker.placeResult.place_id },
-        function(place, status) {
-            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                return;
-            }
-            infoWindow.open(map, marker);
-            buildIWContent(place);
-        });
-}
-
-// Load the place information into the HTML elements used by the info window.
-function buildIWContent(place) {
-    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
-        'src="' + place.icon + '"/>';
-    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
-        '">' + place.name + '</a></b>';
-    document.getElementById('iw-address').textContent = place.vicinity;
-
-    if (place.formatted_phone_number) {
-        document.getElementById('iw-phone-row').style.display = '';
-        document.getElementById('iw-phone').textContent =
-            place.formatted_phone_number;
-    }
-    else {
-        document.getElementById('iw-phone-row').style.display = 'none';
     }
 
-    // Assign a five-star rating to the hotel, using a black star ('&#10029;')
-    // to indicate the rating the hotel has earned, and a white star ('&#10025;')
-    // for the rating points not achieved.
-    if (place.rating) {
-        var ratingHtml = '';
-        for (var i = 0; i < 5; i++) {
-            if (place.rating < (i + 0.5)) {
-                ratingHtml += '&#10025;';
-            }
-            else {
-                ratingHtml += '&#10029;';
-            }
-            document.getElementById('iw-rating-row').style.display = '';
-            document.getElementById('iw-rating').innerHTML = ratingHtml;
-        }
-    }
-    else {
-        document.getElementById('iw-rating-row').style.display = 'none';
-    }
-
-    // The regexp isolates the first part of the URL (domain plus subdomain)
-    // to give a short URL for displaying in the info window.
-    if (place.website) {
-        var fullUrl = place.website;
-        var website = hostnameRegexp.exec(place.website);
-        if (website === null) {
-            website = 'http://' + place.website + '/';
-            fullUrl = website;
-        }
-        document.getElementById('iw-website-row').style.display = '';
-        document.getElementById('iw-website').textContent = website;
-    }
-    else {
-        document.getElementById('iw-website-row').style.display = 'none';
-    }
+    setupClickListener('changecountry-jap', 'JP');
+    setupClickListener('changecountry-usa', 'us');
+    setupClickListener('changecountry-bra', 'BR');
+    setupClickListener('changecountry-gbp', 'GB');
+    setupClickListener('changecountry-saf', 'ZA');
+    setupClickListener(
+        'changecountry-all', ['JP', 'US', 'BR', 'GB', 'ZA']);
 }
