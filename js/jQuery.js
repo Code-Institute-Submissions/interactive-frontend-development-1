5f5a1cd649ce6d517a45b8da075f617d603c5f2a
Marius Dumitru <creion.m@gmail.com>
2019-04-08 19:38:19 +0000

    added the test files

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
+    
     $("#ct-form").css("height", "216px");
+    
+    $("#cont-form").css("display", "none");
     
     $("nav p a").click(function() {
         $("#cont-form").fadeToggle();
