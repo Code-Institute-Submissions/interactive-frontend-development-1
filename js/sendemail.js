ec5533db63c74c0bb83294ac56f151128ba081cb
Marius Dumitru <creion.m@gmail.com>
2019-04-15 10:45:33 +0000

    fixed bug with sending email

--- js/sendemail.js
+++ js/sendemail.js
@@ -1,14 +1,14 @@
 function sendMail(form) {
     emailjs.send("gmail", "milestoneproject", {
-            "from_name": form.name.value,
-            "from_email": form.emailaddress.value,
-            "Request": form.projectsummary.value
-        })
-        .then(
-            function(response) {
-                console.log('success', response);
-            },
-            function(error) {
-                console.log("FAILED", error);
-            });
+        "from_name": form.name.value,
+        "from_email": form.emailaddress.value,
+        "Request": form.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log('success', response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
 }
