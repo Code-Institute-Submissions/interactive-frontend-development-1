b16969b5c94997b6914aacc2e493eb7d4a79a443
Marius Dumitru <creion.m@gmail.com>
2019-04-21 12:21:57 +0000

    changed a few styles for better UX

--- js/sendemail.js
+++ js/sendemail.js
@@ -1,14 +1,16 @@
 function sendMail(form) {
     emailjs.send("gmail", "milestoneproject", {
-        "from_name": form.name.value,
-        "from_email": form.emailaddress.value,
-        "Request": form.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log('success', response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
+            "from_name": form.name.value,
+            "from_email": form.emailaddress.value,
+            "Request": form.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log('success', response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+        return false;
 }
